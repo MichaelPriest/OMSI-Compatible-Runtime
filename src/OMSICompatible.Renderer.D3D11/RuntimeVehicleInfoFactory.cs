e41183a919c3323dc95d690fdb6f874d0286dedf
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-24 10:27:41 -0300

    feat(preview): map trailer origin sound and wheel metadata

--- src/OMSICompatible.Renderer.D3D11/RuntimeVehicleInfoFactory.cs
+++ src/OMSICompatible.Renderer.D3D11/RuntimeVehicleInfoFactory.cs
@@ -277,14 +277,20 @@ public static class RuntimeVehicleInfoFactory
                             -section.JointX,
                             section.JointZ,
                             section.JointY,
+                            -section.OriginX,
+                            section.OriginZ,
+                            section.OriginY,
                             section.FollowerLengthMeters,
                             section.MaximumYawDegrees,
                             section.Reverse,
+                            section.SoundConfigPath,
+                            section.OpenForSound,
                             section.MassTonnes,
                             section.YawInertiaTonneSquareMeters,
                             section.RotationPointLongitudinalMeters,
                             section.WheelBaseMeters,
-                            section.RollingResistanceNewtons))
+                            section.RollingResistanceNewtons,
+                            section.AverageWheelDiameterMeters))
                 .ToArray());
     }
 
